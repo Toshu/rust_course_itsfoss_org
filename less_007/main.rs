347cd487d32afe5a5d4565b3a54c69f3367c4b27
Toshu Amashu <toshude@yahoo.de>
2023-05-29 20:38:52 +0200

    lesson 7: loops

---
+++ less_007/main.rs
@@ -0,0 +1,81 @@
+// LOOPS
+
+fn main() {
+
+    // for loops
+
+    // let my_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    // println!("iteration over an array");
+    // for element in my_arr {
+    //     println!("{}", element);
+    // }
+
+    // println!("\niteration over a real iterator");
+    // for element in my_arr.iter() {
+    //     println!("{}", element);
+    // }
+
+    // println!("\nPython-style range");
+    // for element in 0..10 {
+    //     println!("{}", element);
+    // }
+
+
+
+    // while loops
+
+    // let mut var = 0;
+
+    // while var < 3 {
+    //     println!("{var}");
+    //     var += 1;
+    // }
+
+
+
+    // loop loops
+
+    // let mut var = 0;
+
+    // loop {
+    //     if var > 3 {
+    //         break;
+    //     }
+
+    //     println!("{}", var);
+    //     var += 1;
+    // }
+
+
+
+    // continue
+
+    // for i in 0..10 {
+    //     if i % 2 == 0 {
+    //         continue;
+    //     }
+    //     println!("{}", i)
+    // }
+
+
+
+    // loop with label
+
+    // let mut a = 0;
+    // let mut b = 0;
+
+    // 'parent: loop {
+    //     a += 1;
+
+    //     loop {
+    //         println!("a: {}, b: {}", a, b);
+    //         b += 1;
+
+    //         if a + b == 10 {
+    //             println!("\n{} + {} = 10", a, b);
+    //             break 'parent;
+    //         }
+    //     }
+    // }
+}
